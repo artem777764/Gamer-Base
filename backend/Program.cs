a410145d1d66dbaad32ea5b7468a083d9281eba3
artem777764 <artem200513286@gmail.com>
2025-05-08 14:30:36 +0300

    Начальная конфигурация + Swagger

---
+++ backend/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//swagger: http://localhost:5007/swagger/index.html
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Gamer Base API",
+        Version = "v1"
+    });
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = false;
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gamer Base API");
+    });
+}
+
+app.UseRouting();
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
