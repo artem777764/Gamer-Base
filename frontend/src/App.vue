9ada32b82c8d86b899c669f4c96354fcb5a28c16
artem777764 <artem200513286@gmail.com>
2025-05-20 00:36:56 +0300

    Vue + Tailwind - Базовые Настройки

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TestComponent from "@/components/TestComponent.vue";
+</script>
+
+<template>
+  <TestComponent/>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const counter = ref(0)
+const increment = () => counter.value++
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <button
+      @click="increment"
+      class="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-200"
+    >
+      Счётчик: {{ counter }}
+    </button>
+  </div>
+</template>
