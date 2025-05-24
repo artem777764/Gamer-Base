9b40e4ced099dc80bcbc66e8483535f00fd7fb9d
artem777764 <artem200513286@gmail.com>
2025-05-24 14:20:51 +0300

    Страница Игры

--- frontend/src/pages/GameInfoPage.vue
+++ frontend/src/pages/GameInfoPage.vue
@@ -3,13 +3,21 @@ import { onMounted, ref } from 'vue';
 import axios from 'axios'
 import GameCard from '@/components/GameCard.vue';
 import { useRoute } from 'vue-router';
+import TextBlock from '@/components/TextBlock.vue';
 
 const route = useRoute()
 const id = route.params.id as string
 
 interface Game {
   Id: number
+  Name: string
+  Description: string
+  Genres: [string]
+  Developer: string
+  Publisher: string
   ImageURL: string
+  Platforms: [string]
+  ReleaseDate: Date
 }
 
 const game = ref<Game>()
@@ -30,8 +38,19 @@ onMounted(() => {
 
 <template>
     <div class="min-h-screen bg-background px-5 pt-5">
-        <div v-if="game">
-            <GameCard :image-src="game.ImageURL" class="w-1/4" />
+        <p class="font-russo leading-none text-white text-5xl text-justify">{{game?.Name}}</p>
+        <div class="flex flex-row gap-5 mt-5">
+            <div v-if="game" class="w-1/4 flex-shrink-0">
+                <GameCard :image-src="game.ImageURL"/>
+            </div>
+            <TextBlock>
+                <p class="font-russo leading-none text-white text-3xl text-justify">{{ game?.Description }}</p>
+                <p class="font-russo leading-none text-white text-3xl text-justify">Жанры: {{ game?.Genres.join(', ') }}</p>
+                <p class="font-russo leading-none text-white text-3xl text-justify">Разработчик: {{ game?.Developer }}</p>
+                <p class="font-russo leading-none text-white text-3xl text-justify">Издатель: {{ game?.Publisher }}</p>
+                <p class="font-russo leading-none text-white text-3xl text-justify">Платформы: {{ game?.Platforms.join(', ') }}</p>
+                <p class="font-russo leading-none text-white text-3xl text-justify">Дата выхода: {{ game?.ReleaseDate }}</p>
+            </TextBlock>
         </div>
     </div>
 </template>
